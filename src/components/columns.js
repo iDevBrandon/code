18e4e5d110f6a4329623ab7e9d18d8ac4c96f443
Brandon Ha <idevbrandon@gmail.com>
2022-04-07 11:55:20 +0900

    Building a basic table

---
+++ src/components/columns.js
@@ -0,0 +1,43 @@
+export const COLUMNS = [
+  {
+    Header: "번호",
+    accessor: "id",
+  },
+  {
+    Header: "상태",
+    accessor: "status",
+  },
+  {
+    Header: "등급",
+    accessor: "grade",
+  },
+  {
+    Header: "아이디",
+    accessor: "userid",
+  },
+  {
+    Header: "고객명",
+    accessor: "customer_name",
+  },
+  {
+    Header: "이메일",
+    accessor: "email",
+  },
+  {
+    Header: "주소",
+    accessor: "address",
+  },
+  {
+    Header: "전화번호",
+    accessor: "phone",
+  },
+  {
+    Header: "최근접속일",
+    accessor: "date",
+  },
+  {
+    Header: "관리",
+    accessor: "manage",
+    Cell: ({ cell }) => <button>조회</button>,
+  },
+];
