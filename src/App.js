ead2e4f246004b5616115dd3f8b5f32205a2f780
Brandon Ha <idevbrandon@gmail.com>
2022-04-07 12:04:29 +0900

    basic setting

--- src/App.js
+++ src/App.js
@@ -3,8 +3,20 @@ import BasicTable from "./components/BasicTable";
 
 function App() {
   return (
-    <div className="App">
-      <BasicTable />
+    <div className="App" style={{ display: "flex", width: "100%" }}>
+      <div style={{ width: "15%", backgroundColor: "#4C4C4C" }}>Sidebar</div>
+      <div style={{ width: "100%" }}>
+        <div>
+          <h1>Search Tab</h1>
+          <form>
+            <input />
+            <input />
+          </form>
+        </div>
+        <div style={{ display: "flex", justifyContent: "end" }}>
+          <BasicTable />
+        </div>
+      </div>
     </div>
   );
 }
